18a90a70779ad536f002840455ae751718fff734
Cicicai379 <caiyuxin379@gmail.com>
2023-02-02 14:39:10 +0800

    cici

---
+++ src/User.java
@@ -0,0 +1,54 @@
+public class User {
+    private int progress;
+    private int rank;
+
+    public User(){
+        this.rank=-8;
+        this.progress=0;
+    }
+
+    public int getProgress() {
+        return progress;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+    public void incProgress(int r){
+        if(r<-8 ||r>8||r==0){
+            throw new IllegalArgumentException("The rank of an activity cannot be less than 8, 0, or greater than 8!");
+        }
+        System.out.println(rank+" "+r+" "+progress);
+        if(r==rank){
+            progress+=3;
+        }else if(r<rank-1 &&!(rank>0 && r<0)){
+            progress+=0;
+        }else if(r<rank){
+//            rankif (rank>0 && r<0){
+//                r+=1;
+//            }
+            progress+=1;
+        }else{
+            if(rank<0 && r>0){
+                r-=1;
+            }
+            r = r-this.rank;
+
+            this.progress+=r*r*10;
+
+        }
+
+        while(progress>=100){
+
+            progress-=100;
+            rank+=1;
+            if(rank==0) rank+=1;
+        }
+
+    }
+
+
+    public String toString() {
+        return "User{" + "rank=" + this.rank + ", progress=" + this.progress + '}';
+    }
+}
